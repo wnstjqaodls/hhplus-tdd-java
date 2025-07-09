59504c542a89164afc972cd1c52b5b57a035301f
Seobkim-Desktop-Win <wnstjqaodls@gmail.com>
2025-07-10 08:40:47 +0900

    :dependency: 의존성 파일 오류 수정-2

--- src/main/java/io/hhplus/tdd/point/PointHistory.java
+++ src/main/java/io/hhplus/tdd/point/PointHistory.java
@@ -1,14 +1,10 @@
 package io.hhplus.tdd.point;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class PointHistory {
-    private final long id;
-    private final long userId;
-    private final long amount;
-    private final TransactionType type;
-    private final long updateMillis;
+public record PointHistory(
+        long id,
+        long userId,
+        long amount,
+        TransactionType type,
+        long updateMillis
+) {
 }
